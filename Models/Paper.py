ee0b67b0a8941d4f606411f70da4f3addc644614
Daniel Guldberg Aaes <guldberg72@gmail.com>
2019-11-09 17:13:56 +0100

    gitignore has been added

---
+++ Models/Paper.py
@@ -0,0 +1,9 @@
+class Paper:
+    def __init__(self, author, creator, producer, subject, title, file_name, path):
+        self.author = author
+        self.creator = creator
+        self.producer = producer
+        self.subject = subject
+        self.title = title
+        self.file_name = file_name
+        self.path = path
