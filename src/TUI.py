ccd3e2a0a7af8a323f57ca5e8b75734a8c806485
jakob1379 <jakob1379@gmail.com>
2019-11-12 21:08:37 +0100

    cleanup and and correction to standard

--- src/TUI.py
+++ src/TUI.py
@@ -1,12 +1,11 @@
 # https://raw.githubusercontent.com/nikhilkumarsingh/python-curses-tut/master/code/example5.py
-
 import curses
-from Papers import *
+from Papers import get_info, get_files
 
 # menu = ['Home', 'Play', 'Scoreboard', 'Exit']
 menu = get_files() + ['Exit']
 
-def print_menu(stdscr, selected_row_idx):
+def printMenu(stdscr, selected_row_idx):
     stdscr.clear()
     h, w = stdscr.getmaxyx()
     for idx, row in enumerate(menu):
@@ -18,10 +17,10 @@ def print_menu(stdscr, selected_row_idx):
             stdscr.attroff(curses.color_pair(1))
         else:
             stdscr.addstr(y, x, row)
-            stdscr.refresh()
+    stdscr.refresh()
 
 
-def print_center(stdscr, text):
+def printCenter(stdscr, text):
     stdscr.clear()
     h, w = stdscr.getmaxyx()
     x = w//2 - len(text)//2
@@ -29,24 +28,26 @@ def print_center(stdscr, text):
     stdscr.addstr(y, x, text)
     stdscr.refresh()
 
-def drawRrectangle(win, corners):                                         
-    """Draw a rectangle with corners at the provided upper-left                 
-    and lower-right coordinates.                                                
-    """                                                                         
-    (ulx,uly,lrx,lry) = corners 
-    win.vline(uly+1, ulx, curses.ACS_VLINE, lry - uly - 1)                      
-    win.hline(uly, ulx+1, curses.ACS_HLINE, lrx - ulx - 1)                      
-    win.hline(lry, ulx+1, curses.ACS_HLINE, lrx - ulx - 1)                      
-    win.vline(uly+1, lrx, curses.ACS_VLINE, lry - uly - 1)                      
-    win.addch(uly, ulx, curses.ACS_ULCORNER)                                    
-    win.addch(uly, lrx, curses.ACS_URCORNER)                                    
-    win.addch(lry, lrx, curses.ACS_LRCORNER)                                    
+
+def printRectangle(win):
+    """Draw a rectangle with corners at the provided upper-left
+    and lower-right coordinates.
+    """
+    (ulx, uly, lrx, lry) = getCorners(win)
+    win.vline(uly+1, ulx, curses.ACS_VLINE, lry - uly - 1)
+    win.hline(uly, ulx+1, curses.ACS_HLINE, lrx - ulx - 1)
+    win.hline(lry, ulx+1, curses.ACS_HLINE, lrx - ulx - 1)
+    win.vline(uly+1, lrx, curses.ACS_VLINE, lry - uly - 1)
+    win.addch(uly, ulx, curses.ACS_ULCORNER)
+    win.addch(uly, lrx, curses.ACS_URCORNER)
+    win.addch(lry, lrx, curses.ACS_LRCORNER)
     win.addch(lry, ulx, curses.ACS_LLCORNER)
+
 
 def getCorners(stdscr):
     uly = (int(stdscr.getbegyx()[0]))
     ulx = (int(stdscr.getbegyx()[1]))
-    lry = (int(stdscr.getmaxyx()[0] * 0.7))
+    lry = (int(stdscr.getmaxyx()[0]* 0.5))
     lrx = (int(stdscr.getmaxyx()[1] - 1))
 
     return ulx, uly, lrx, lry
@@ -63,17 +64,17 @@ def main(stdscr):
     current_row = 0
 
     # print the menu
-    print_menu(stdscr, current_row)
-    
+    printMenu(stdscr, current_row)
+
     while 1:
         key = stdscr.getch()
-        
+
         if key == curses.KEY_UP and current_row > 0:
             current_row -= 1
         elif key == curses.KEY_DOWN and current_row < len(menu)-1:
             current_row += 1
         elif key == curses.KEY_ENTER or key in [10, 13]:
-            print_center(stdscr, "You selected '{}'".format(menu[current_row]))
+            printCenter(stdscr, "You selected '{}'".format(menu[current_row]))
             stdscr.getch()
             current_file = menu[current_row]
             # if user selected last row, exit the program
@@ -81,7 +82,7 @@ def main(stdscr):
                 break
 
 
-        print_menu(stdscr, current_row)
+        printMenu(stdscr, current_row)
 
 
 curses.wrapper(main)
